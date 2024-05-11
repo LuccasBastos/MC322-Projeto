043997d13b72e7dc7ff6b36f30ab23d39a84e611
Luccas Bastos <62710085+LuccasBastos@users.noreply.github.com>
2024-05-11 01:12:55 -0300

    tree Campeonato v0

--- Proj_1/src/Proj_1/MataMata.java
+++ Proj_1/src/Proj_1/MataMata.java
@@ -1,5 +1,29 @@
 package Proj_1;
+import java.util.ArrayList;
 
-public class MataMata {
+public class MataMata extends Campeonato{
+	ArrayList<Jogo> jogos = new ArrayList<Jogo>();
 
+	public MataMata(String nome) {
+		super(nome);
+	}
+	
+	public void addConfronto(Time timeA, Time timeB) {
+		jogos.add(null);
+	}
+	
+	public void fazConfrontos(ArrayList<Time> classificados){		
+		for (int i = 0; i < classificados.size()/2; i++) {
+			addConfronto(classificados.get(i),classificados.get(classificados.size()));
+			classificados.remove(i);
+			classificados.remove(classificados.size());
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "";
+	}
+	
+	
 }
