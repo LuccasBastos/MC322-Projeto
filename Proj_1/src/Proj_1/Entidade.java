a58267090edc198d62a50b1ef87844611a5fc4a1
TaniTani06 <gustavoandradetaniguchi@gmail.com>
2024-05-12 11:14:41 -0300

    [ADDED] properties and constructor to Entidade

--- Proj_1/src/Proj_1/Entidade.java
+++ Proj_1/src/Proj_1/Entidade.java
@@ -1,5 +1,26 @@
 package Proj_1;
+import java.util.ArrayList;
 
 public class Entidade {
+    private static int count = 0;
+    private String nome;
+    private int id;
+    private ArrayList<Integer> esportes;
+    private Pessoa resp;
 
+    //Construtor "completo"
+    public Entidade(String nome, ArrayList<Integer> esportes, Pessoa resp){
+        this.id = count++;
+        this.nome = nome;
+        this.esportes = esportes;
+        this.resp = resp;
+    }
+
+    //Construtor somente com nome da Entidade e responsável
+    public Entidade(String nome, Pessoa resp){
+        this.id = count++;
+        this.nome = nome;
+        this.esportes = new ArrayList<>();
+        this.resp = resp;
+    }
 }
