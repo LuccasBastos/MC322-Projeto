537572e63dab474acedc8bb78ad1c4f534a7c097
CH-bruno <brunogg565@gmail.com>
2024-05-12 19:23:14 -0300

    Versão 1 da arvore Pessoa

--- Proj_1/src/Proj_1/Arbitragem.java
+++ Proj_1/src/Proj_1/Arbitragem.java
@@ -1,5 +1,50 @@
 package Proj_1;
+import java.util.ArrayList;
 
-public class Arbitragem {
+public class Arbitragem extends Pessoa {
+    private ArrayList<String> nomeMesa;
 
+    public Arbitragem(String nome, String cpf, Entidade entidade, String foto, ArrayList<String> nomeMesa) {
+        super(nome, cpf, entidade, foto);
+        this.nomeMesa = nomeMesa;
+    }
+
+    public ArrayList<String> getNomeMesa() {
+        return nomeMesa;
+    }
+
+    @Override
+    public String obterNome() {
+        return nome;
+    }
+
+    @Override
+    public String obterCpf() {
+        return cpf;
+    }
+
+    @Override
+    public Entidade obterEntidade() {
+        return entidade;
+    }
+
+    @Override
+    public String obterFoto() {
+        return foto;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Nome: ").append(nome).append("\n");
+        sb.append("CPF: ").append(cpf).append("\n");
+        sb.append("Entidade: ").append(entidade).append("\n");
+        sb.append("Foto: ").append(foto).append("\n");
+        sb.append("Nome da Mesa: ");
+        for (String nomeArbitro : nomeMesa) {
+            sb.append(nomeArbitro).append(", ");
+        }
+        sb.delete(sb.length() - 2, sb.length()); // Remover a vírgula extra no final
+        return sb.toString();
+    }
 }
