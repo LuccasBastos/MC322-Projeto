98aab99cfd3ecdc51547dcfb96d6c69d1d150373
TaniTani06 <gustavoandradetaniguchi@gmail.com>
2024-05-19 14:21:51 -0300

    [FIXED] Camp declaration

--- Proj_1/src/Proj_1/Main.java
+++ Proj_1/src/Proj_1/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		//variáveis:
 		String nomeCamp;
-		FaseGrupos Camp;
+		Campeonato Camp;
 		int qntEntidades;
 		int escolhaentidade;
 		int qntEsportes;
